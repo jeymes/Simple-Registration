7b6cd0cba0fea0d9616b66779c1b2a5d562fba4c
jeymes <jsvpn2017@gmail.com>
2022-07-07 19:20:14 -0300

    criando estrutura

---
+++ src/pages/Pages.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as J from './Pages.styles'
+
+export default function Home() {
+  return (
+    <div>
+    <J.Title>jeymes</J.Title>
+  </div>
+  )
+}
